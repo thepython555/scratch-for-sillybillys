52a763265698bbf89dfd30036e4951ebc500cdfc
Eruption <94153406+EruptionGuy@users.noreply.github.com>
2024-03-03 21:56:48 +0700

    Update save-load.js
    Remove "Hey this domain is going to be removed" message

--- src/save-load.js
+++ src/save-load.js
@@ -68,27 +68,27 @@ export default async function register(self) {
 		}, 1000)
 	}
 	/* Domain change swal */
-	if (window.location.hostname == "scratch-for-discord.com") {
-		Swal.fire({
-			title: 'Hey this domain is going to be removed',
-			text: "Do you want to be taken to the new domain?",
-			icon: 'warning',
-			showCancelButton: true,
-			confirmButtonColor: '#3085d6',
-			cancelButtonColor: '#d33',
-			confirmButtonText: 'Yes!',
-			cancelButtonText: 'No.'
-		}).then((result) => {
-			if (result.isConfirmed) {
-				console.log('redirecting...');
-				window.location.assign("https://s4d.discodes.xyz");
-			} else {
-				autoSave()
-			};
-		});
-	} else {
-		autoSave()
-	}
+	// if (window.location.hostname == "scratch-for-discord.com") {
+	// 	Swal.fire({
+	// 		title: 'Hey this domain is going to be removed',
+	// 		text: "Do you want to be taken to the new domain?",
+	// 		icon: 'warning',
+	// 		showCancelButton: true,
+	// 		confirmButtonColor: '#3085d6',
+	// 		cancelButtonColor: '#d33',
+	// 		confirmButtonText: 'Yes!',
+	// 		cancelButtonText: 'No.'
+	// 	}).then((result) => {
+	// 		if (result.isConfirmed) {
+	// 			console.log('redirecting...');
+	// 			window.location.assign("https://s4d.discodes.xyz");
+	// 		} else {
+	// 			autoSave()
+	// 		};
+	// 	});
+	// } else {
+	// 	autoSave()
+	// }
 }
 
 async function handle(workspace) {
